0e1eff4dd6899a4aa070a28b2603d4b402b1941b
Jeremy Scala <scalajeremy@gmail.com>
2019-03-22 12:06:21 +0100

    Updates

---
+++ precache-manifest.8ee0b7472251932029ea500d6a77c7b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-character-manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "46360511b57c2b7ae471",
+    "url": "/react-character-manager/static/js/main.4af75d9d.chunk.js"
+  },
+  {
+    "revision": "c4a0dce47a8be5825ecb",
+    "url": "/react-character-manager/static/js/2.9051aa8e.chunk.js"
+  },
+  {
+    "revision": "46360511b57c2b7ae471",
+    "url": "/react-character-manager/static/css/main.e8a733f3.chunk.css"
+  },
+  {
+    "revision": "c4a0dce47a8be5825ecb",
+    "url": "/react-character-manager/static/css/2.f7ae5d29.chunk.css"
+  },
+  {
+    "revision": "1420e6d4226a8e4fcd5e094ab3f498c5",
+    "url": "/react-character-manager/index.html"
+  }
+];
