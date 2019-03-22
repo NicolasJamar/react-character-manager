929ec7d2d412db03f258676d9326c0a6ce490753
Jeremy Scala <scalajeremy@gmail.com>
2019-03-22 16:53:04 +0100

    working show and some style
    Get the characters and display them, style for background, the cards and some animations

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../css/App.scss';
+import '../css/hover.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 const API = 'https://character-database.becode.xyz/characters';
 
+function random_bg_color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+     return bgColor;
+    }
+random_bg_color();
+
 class App extends Component {
+
+
   constructor(props) {
     super(props);
 
@@ -26,10 +39,10 @@ class App extends Component {
 
     return (
       <React.Fragment>
-      <div className="container mx-auto">
-        <div className="row">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
           {character.map(character =>
-            <div class="card border rounded-lg p-3 m-2" id="card_body_bg" style={{width: 25 + 'rem'}}>
+            <div class="card border-danger p-3 m-2 hvr-buzz" id="card_body_bg" style={{width: 25 + 'rem', background:random_bg_color() }}>
               <div class="card-body">
                 <img src={`data:image/jpeg;base64,${character.image}`} style={{width: 100 + 'px'}} className="rounded-circle float-left mr-5" alt={character.name}/>
                 <h5 class="card-title">{character.name}</h5>
