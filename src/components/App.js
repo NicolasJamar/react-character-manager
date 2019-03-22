6c687a1c5883df1225122a1ff8a4562663da4732
Jeremy Scala <scalajeremy@gmail.com>
2019-03-22 11:38:07 +0100

    add sass and trying some basic function for styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
-import '../css/App.css';
+import '../css/App.scss';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../css/index.css';
+
 
 const API = 'https://character-database.becode.xyz/characters';
 
@@ -26,31 +26,16 @@ class App extends Component {
 
     return (
       <React.Fragment>
-      <div className="container">
+      <div className="container mx-auto">
         <div className="row">
           {character.map(character =>
-            <div class="card" style={{width: 35 + 'rem'}}>
-              <img src={`data:image/jpeg;base64,${character.image}`} style={{width: 100 + 'px'}} className="rounded-circle" alt="..."/>
+            <div class="card border rounded-lg p-3 m-2" id="card_body_bg" style={{width: 25 + 'rem'}}>
               <div class="card-body">
+                <img src={`data:image/jpeg;base64,${character.image}`} style={{width: 100 + 'px'}} className="rounded-circle float-left mr-5" alt={character.name}/>
                 <h5 class="card-title">{character.name}</h5>
                 <p class="card-text">{character.shortDescription}</p>
               </div>
             </div>
-            // <div key={character.id} className="col-sm-6">
-            //   <div className="card text-black bg-light mb-3 p-5">
-            //     <div className="row no-gutters">
-            //       <div className="col-md-4 my-auto">
-            //         <img src={`data:image/jpeg;base64,${character.image}`} className="rounded-circle" alt="..."/>
-            //       </div>
-            //       <div className="col-md-8">
-            //         <div className="card-body">
-            //           <p className="card-text">{character.name}</p>
-            //           <p className="card-text">{character.shortDescription}</p>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </div>
-            // </div>
             )
           }
         </div>
